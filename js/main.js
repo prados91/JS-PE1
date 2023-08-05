f08644e4c11ba360ff87afcccb1c9a03c7e106aa
Augusto Prados <augustoprados91@gmail.com>
2023-08-05 13:15:45 -0300

    Cambios

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Declaración de funciones
+
+
+//Declaración de variables
+let num1=parseFloat(prompt("Ingrese el primer número: "));;
+let num2=parseFloat(prompt("Ingrese el segundo número: "));;
+let num3=parseFloat(prompt("Ingrese el tercer número: "));;
+let num4=parseFloat(prompt("Ingrese el cuarto número: "));;
+let num5=parseFloat(prompt("Ingrese el quinto número: "));;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
